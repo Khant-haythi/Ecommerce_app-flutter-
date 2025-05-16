9be8275a5ce352559ac968f656b784e9f1fa94a8
Khant-haythi <khanthaythi@gmail.com>
2025-05-16 11:08:33 +0630

    Create bottomnavbar.dart

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -7,11 +7,14 @@ class MyBottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(vertical: 20),
       child: GNav(
         color: Colors.grey[400],
           activeColor: Colors.grey.shade700,
           tabActiveBorder: Border.all(color: Colors.white),
           tabBackgroundColor: Colors.grey.shade100,
+          mainAxisAlignment: MainAxisAlignment.center,
+          tabBorderRadius: 16,
           tabs:const [
             GButton(
                 icon: Icons.home,
